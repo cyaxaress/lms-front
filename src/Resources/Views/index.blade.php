1295393a80fea3bb4869a5cfe394c37711f90424
Hemn Movafeghi <hemn791@gmail.com>
2022-03-23 20:44:10 +0300

    index

--- src/Resources/Views/index.blade.php
+++ src/Resources/Views/index.blade.php
@@ -5,8 +5,6 @@
             @include('Front::layout.header-ads')
             @include('Front::layout.top-info')
             @include('Front::layout.latestCourses')
-            @include('Front::layout.popularCourses')
         </article>
-        @include('Front::layout.latestArticles')
         <main id="single">
 @endsection
